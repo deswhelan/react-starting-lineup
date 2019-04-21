4ecca19110817879767fd0027b04d29cd1b6aa87
Des Whelan <wheland6@tcd.ie>
2019-04-21 19:43:59 +1200

    rearrange App.jsx layout

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -105,37 +105,37 @@ class App extends React.Component {
             <div className="top-right-corner"></div>
           </div>
         </a>
-        <button onClick={this.handleFormationClick} id="541">5-4-1</button>
-        <button onClick={this.handleFormationClick} id="532">5-3-2</button>
-        <button onClick={this.handleFormationClick} id="442">4-4-2</button>
-        <button onClick={this.handleFormationClick} id="433">4-3-3</button>
-        <button onClick={this.handleFormationClick} id="424">4-2-4</button>
-        <button onClick={this.handleFormationClick} id="352">3-5-2</button>
-        <button onClick={this.handleFormationClick} id="343">3-4-3</button>
-        <br></br>
-        <button onClick={this.handleResultsClick} id="Arsenal">Arsenal</button>
-        <button onClick={this.handleResultsClick} id="AFC Bournemouth">AFC Bournemouth</button>
-        <button onClick={this.handleResultsClick} id="Brighton &amp; Hove Albion">Brighton and Hove Albion</button>
-        <button onClick={this.handleResultsClick} id="Burnley">Burnley</button>
-        <button onClick={this.handleResultsClick} id="Cardiff City">Cardiff City</button>
-        <button onClick={this.handleResultsClick} id="Chelsea">Chelsea</button>
-        <button onClick={this.handleResultsClick} id="Crystal Palace">Crystal Palace</button>
-        <button onClick={this.handleResultsClick} id="Everton">Everton</button>
-        <button onClick={this.handleResultsClick} id="Fulham">Fulham</button>
-        <button onClick={this.handleResultsClick} id="Huddersfield Town">Huddersfield Town</button>
-        <button onClick={this.handleResultsClick} id="Leicester City">Leicester City</button>
-        <button onClick={this.handleResultsClick} id="Liverpool">Liverpool</button>
-        <button onClick={this.handleResultsClick} id="Manchester City">Manchester City</button>
-        <button onClick={this.handleResultsClick} id="Newcastle United">Newcastle United</button>
-        <button onClick={this.handleResultsClick} id="Southampton">Southampton</button>
-        <button onClick={this.handleResultsClick} id="Tottenham Hotspur">Tottenham Hotspur</button>
-        <button onClick={this.handleResultsClick} id="West Ham United">West Ham United</button>
-        <button onClick={this.handleResultsClick} id="Wolverhampton Wanderers">Wolverhampton Wanderers</button>
         <div className="container">
           <Goalkeeper team={this.state.team}/>
           <Defenders team={this.state.team} numOfStarters={this.state.defendersNeeded}/>
           <Midfielders team={this.state.team} numOfStarters={this.state.midfieldersNeeded}/>
           <Forwards team={this.state.team} numOfStarters={this.state.forwardsNeeded}/>
+          <button onClick={this.handleFormationClick} id="541">5-4-1</button>
+          <button onClick={this.handleFormationClick} id="532">5-3-2</button>
+          <button onClick={this.handleFormationClick} id="442">4-4-2</button>
+          <button onClick={this.handleFormationClick} id="433">4-3-3</button>
+          <button onClick={this.handleFormationClick} id="424">4-2-4</button>
+          <button onClick={this.handleFormationClick} id="352">3-5-2</button>
+          <button onClick={this.handleFormationClick} id="343">3-4-3</button>
+          <br></br>
+          <button onClick={this.handleResultsClick} id="Arsenal">Arsenal</button>
+          <button onClick={this.handleResultsClick} id="AFC Bournemouth">AFC Bournemouth</button>
+          <button onClick={this.handleResultsClick} id="Brighton &amp; Hove Albion">Brighton and Hove Albion</button>
+          <button onClick={this.handleResultsClick} id="Burnley">Burnley</button>
+          <button onClick={this.handleResultsClick} id="Cardiff City">Cardiff City</button>
+          <button onClick={this.handleResultsClick} id="Chelsea">Chelsea</button>
+          <button onClick={this.handleResultsClick} id="Crystal Palace">Crystal Palace</button>
+          <button onClick={this.handleResultsClick} id="Everton">Everton</button>
+          <button onClick={this.handleResultsClick} id="Fulham">Fulham</button>
+          <button onClick={this.handleResultsClick} id="Huddersfield Town">Huddersfield Town</button>
+          <button onClick={this.handleResultsClick} id="Leicester City">Leicester City</button>
+          <button onClick={this.handleResultsClick} id="Liverpool">Liverpool</button>
+          <button onClick={this.handleResultsClick} id="Manchester City">Manchester City</button>
+          <button onClick={this.handleResultsClick} id="Newcastle United">Newcastle United</button>
+          <button onClick={this.handleResultsClick} id="Southampton">Southampton</button>
+          <button onClick={this.handleResultsClick} id="Tottenham Hotspur">Tottenham Hotspur</button>
+          <button onClick={this.handleResultsClick} id="West Ham United">West Ham United</button>
+          <button onClick={this.handleResultsClick} id="Wolverhampton Wanderers">Wolverhampton Wanderers</button>
           <HeadToHead results={this.state.results}/>
         </div>
       </div>

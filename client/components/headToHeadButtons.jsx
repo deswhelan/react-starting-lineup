0e3d437034841c8629ab1e082435d6c9362d980c
Des Whelan <wheland6@tcd.ie>
2019-04-23 21:22:59 +1200

    style h2h results (incomplete)

--- client/components/headToHeadButtons.jsx
+++ client/components/headToHeadButtons.jsx
@@ -8,25 +8,26 @@ class HeadToHeadButtons extends React.Component {
     render () {
         if(this.props.display){
             return (
-                <div className="HeadToHeadButtons">
-                <button onClick={this.handleHeadToHeadClick} id="Arsenal">Arsenal</button>
-                <button onClick={this.handleHeadToHeadClick} id="AFC Bournemouth">AFC Bournemouth</button>
-                <button onClick={this.handleHeadToHeadClick} id="Brighton &amp; Hove Albion">Brighton and Hove Albion</button>
-                <button onClick={this.handleHeadToHeadClick} id="Burnley">Burnley</button>
-                <button onClick={this.handleHeadToHeadClick} id="Cardiff City">Cardiff City</button>
-                <button onClick={this.handleHeadToHeadClick} id="Chelsea">Chelsea</button>
-                <button onClick={this.handleHeadToHeadClick} id="Crystal Palace">Crystal Palace</button>
-                <button onClick={this.handleHeadToHeadClick} id="Everton">Everton</button>
-                <button onClick={this.handleHeadToHeadClick} id="Fulham">Fulham</button>
-                <button onClick={this.handleHeadToHeadClick} id="Huddersfield Town">Huddersfield Town</button>
-                <button onClick={this.handleHeadToHeadClick} id="Leicester City">Leicester City</button>
-                <button onClick={this.handleHeadToHeadClick} id="Liverpool">Liverpool</button>
-                <button onClick={this.handleHeadToHeadClick} id="Manchester City">Manchester City</button>
-                <button onClick={this.handleHeadToHeadClick} id="Newcastle United">Newcastle United</button>
-                <button onClick={this.handleHeadToHeadClick} id="Southampton">Southampton</button>
-                <button onClick={this.handleHeadToHeadClick} id="Tottenham Hotspur">Tottenham Hotspur</button>
-                <button onClick={this.handleHeadToHeadClick} id="West Ham United">West Ham United</button>
-                <button onClick={this.handleHeadToHeadClick} id="Wolverhampton Wanderers">Wolverhampton Wanderers</button>
+                <div className="head-to-head-buttons">
+                <h4>Choose opponent:</h4>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Arsenal">Arsenal</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="AFC Bournemouth">AFC Bournemouth</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Brighton &amp; Hove Albion">Brighton and Hove Albion</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Burnley">Burnley</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Cardiff City">Cardiff City</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Chelsea">Chelsea</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Crystal Palace">Crystal Palace</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Everton">Everton</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Fulham">Fulham</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Huddersfield Town">Huddersfield Town</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Leicester City">Leicester City</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Liverpool">Liverpool</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Manchester City">Manchester City</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Newcastle United">Newcastle United</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Southampton">Southampton</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Tottenham Hotspur">Tottenham Hotspur</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="West Ham United">West Ham United</button>
+                <button onClick={this.handleHeadToHeadClick} className="head-to-head-button" id="Wolverhampton Wanderers">Wolverhampton Wanderers</button>
                 </div>
         )} else return <div></div>
     }
